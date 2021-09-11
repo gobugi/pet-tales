316e38604cf2f03f1cd3bc0009919cd56b284670
johnny <johnpark1987@gmail.com>
2021-09-11 02:20:31 -0700

    finished user login api route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
-const { requireAuth, restoreUser, setTokenCookie } = require('../../utils/auth.js');
-const asyncHandler = require('express-async-handler');
-const { User } = require('../../db/models');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
 
+router.post('/test', (req, res) => {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
