91e8071466d76ebda19e280626e2b2b240684c61
johnny <johnpark1987@gmail.com>
2021-09-11 14:21:36 -0700

    finished frontend phase 0

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+// const { User } = require('../../db/models');
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
 
 module.exports = router;
