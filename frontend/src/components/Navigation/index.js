33cff369f83f61cb7c5f9cf6baa6cf2e818eddc9
johnny <johnpark1987@gmail.com>
2021-09-12 02:10:27 -0700

    navbar

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -15,8 +15,8 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        <NavLink to="/login">Log In</NavLink>
-        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink className="navButton" to="/login">Log In</NavLink>
+        <NavLink className="navButton" to="/signup">Sign Up</NavLink>
       </>
     );
   }
@@ -24,7 +24,7 @@ function Navigation({ isLoaded }){
   return (
     <ul style={{listStyleType: 'none'}}>
       <li>
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink className="navHome" exact to="/">Home</NavLink>
         {isLoaded && sessionLinks}
       </li>
     </ul>
