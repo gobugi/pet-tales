ccfedd094aaacada7479f728b5920a1aedbdd0ff
johnny <johnpark1987@gmail.com>
2021-09-13 23:34:55 -0700

    created/seeded Stories table

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -48,6 +48,7 @@ module.exports = (sequelize, DataTypes) => {
   });
   User.associate = function(models) {
     // associations can be defined here
+    User.hasMany(models.Story, {foreignKey: 'authorId'})
   };
 
   User.prototype.toSafeObject = function() { // remember, this cannot be an arrow function
