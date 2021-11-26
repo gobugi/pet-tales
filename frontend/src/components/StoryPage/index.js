b6e0f94483dede65350c65b4c467fca2f74af708
johnny <johnpark1987@gmail.com>
2021-11-26 14:40:18 -0800

    all likes works now

--- frontend/src/components/StoryPage/index.js
+++ frontend/src/components/StoryPage/index.js
@@ -7,6 +7,7 @@ import { restoreUser } from '../../store/session';
 import { getComments } from '../../store/comments';
 import StoryFormPage from '../StoryFormPage';
 import './StoryPage.css';
+import { csrfFetch } from "../../store/csrf";
 
 import CommentsFromAStory from '../CommentsFromAStory';
 
@@ -48,6 +49,210 @@ const StoryPage = () => {
 
   const id = +storyId
 
+  const [allLikes, setAllLikes] = useState([]);
+  const [currentLike, setCurrentLike] = useState([]);
+
+//////////////// display total likes and relevant heart ///////////////////
+  useEffect(() => {
+    async function all_likes() {
+			const response = await fetch(`/api/likes/`);
+			const responseData = await response.json();
+
+      const currStoryLikesArr = [];
+      responseData?.forEach(like => {
+        like?.storyId === id && currStoryLikesArr?.push(like)
+
+        if (like?.storyId === id && like?.userId === currentUserId) {
+          setCurrentLike(like);
+        }
+      })
+
+      setAllLikes(currStoryLikesArr);
+
+        const checkLikedArr = [];
+        currStoryLikesArr?.forEach(like => {
+          if (like?.userId === sessionUser?.id) {
+            document.getElementById("loggedFullHeart")?.setAttribute("style","display:block;");
+            document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:none;");
+            document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:none;");
+            document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:none;");
+            checkLikedArr?.push(like)
+          }
+        })
+
+        if (!checkLikedArr?.length) {
+          document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:block;");
+          document.getElementById("loggedFullHeart")?.setAttribute("style","display:none;");
+          document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:none;");
+          document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:none;");
+        }
+
+      if (sessionUser === undefined && !currStoryLikesArr?.length) {
+        document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:block;");
+        document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedFullHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:none;");
+      }
+
+      if (sessionUser === undefined && currStoryLikesArr?.length) {
+        document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:block;");
+        document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedFullHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:none;");
+      }
+
+		}
+    all_likes();
+	}, []);
+
+  /////////// add heart ////////////
+
+  const addLike = async (e) => {
+    e.preventDefault()
+
+    const newLike = {
+      "userId": currentUserId,
+      "storyId": id
+    }
+
+    const response = await fetch(`/api/likes/`, {
+      method: 'POST',
+      body: JSON.stringify(newLike),
+      headers: {
+        "Content-Type": "application/json"
+      }
+
+    });
+
+
+    if (response.ok) {
+      document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:none;");
+      document.getElementById("loggedFullHeart")?.setAttribute("style","display:block;");
+    }
+
+
+
+    async function all_likes() {
+      const response = await fetch(`/api/likes/`);
+      const responseData = await response.json();
+
+      const currStoryLikesArr = [];
+      responseData?.forEach(like => {
+        like?.storyId === id && currStoryLikesArr?.push(like)
+
+        if (like?.storyId === id && like?.userId === currentUserId) {
+          setCurrentLike(like);
+        }
+
+      })
+
+      setAllLikes(currStoryLikesArr);
+
+        const checkLikedArr = [];
+        currStoryLikesArr?.forEach(like => {
+          if (like?.userId === sessionUser?.id) {
+            document.getElementById("loggedFullHeart")?.setAttribute("style","display:block;");
+            document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:none;");
+            document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:none;");
+            document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:none;");
+            checkLikedArr?.push(like)
+          }
+        })
+
+        if (!checkLikedArr?.length) {
+          document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:block;");
+          document.getElementById("loggedFullHeart")?.setAttribute("style","display:none;");
+          document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:none;");
+          document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:none;");
+        }
+
+      if (sessionUser === undefined && !currStoryLikesArr?.length) {
+        document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:block;");
+        document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedFullHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:none;");
+      }
+
+      if (sessionUser === undefined && currStoryLikesArr?.length) {
+        document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:block;");
+        document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedFullHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:none;");
+      }
+
+		}
+    all_likes();
+
+  }
+
+
+  const remLike = async (e) => {
+    e.preventDefault();
+    await fetch(`/api/likes/${currentLike?.id}`, {
+        method: 'DELETE'
+    })
+
+    setCurrentLike([]);
+
+    async function all_likes() {
+      const response = await fetch(`/api/likes/`);
+      const responseData = await response.json();
+
+      const currStoryLikesArr = [];
+      responseData?.forEach(like => {
+        like?.storyId === id && currStoryLikesArr?.push(like)
+
+        if (like?.storyId === id && like?.userId === currentUserId) {
+          setCurrentLike(like);
+        }
+
+      })
+
+      setAllLikes(currStoryLikesArr);
+
+        const checkLikedArr = [];
+        currStoryLikesArr?.forEach(like => {
+          if (like?.userId === sessionUser?.id) {
+            document.getElementById("loggedFullHeart")?.setAttribute("style","display:block;");
+            document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:none;");
+            document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:none;");
+            document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:none;");
+            checkLikedArr?.push(like)
+          }
+        })
+
+        if (!checkLikedArr?.length) {
+          document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:block;");
+          document.getElementById("loggedFullHeart")?.setAttribute("style","display:none;");
+          document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:none;");
+          document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:none;");
+        }
+
+      if (sessionUser === undefined && !currStoryLikesArr?.length) {
+        document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:block;");
+        document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedFullHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:none;");
+      }
+
+      if (sessionUser === undefined && currStoryLikesArr?.length) {
+        document.getElementById("loggedOutFullHeart")?.setAttribute("style","display:block;");
+        document.getElementById("loggedOutEmptyHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedFullHeart")?.setAttribute("style","display:none;");
+        document.getElementById("loggedEmptyHeart")?.setAttribute("style","display:none;");
+      }
+
+		}
+    all_likes();
+
+  };
+
+
+
+
+
+
+
   useEffect(() => {
     dispatch(getStories());
     dispatch(getUsers());
@@ -80,8 +285,20 @@ const StoryPage = () => {
                 <td className="smallStoryTd">
                   <table>
                     <tr><td className="smallStoryTitle">{`${currentStory?.title}`}</td></tr>
-                    <tr><a className="smallStoryAuthor" href={`/users/${userId}`}><td className="smallStoryAuthor">{`by ${currentUser?.username}`}</td></a></tr>
+                    <tr>
+                      <a className="smallStoryAuthor" href={`/users/${userId}`}>
+                        <td className="smallStoryAuthor">{`by ${currentUser?.username}`}</td>
+                      </a>
+                    </tr>
                     <tr><td className="smallStoryBody">{`${currentStory?.body}`}</td></tr>
+                    <br />
+                    <tr className="likesContainer">
+                      <i className="far fa-heart" id="loggedEmptyHeart" onClick={addLike} style={{display:"none"}}></i>
+                      <i className="fas fa-heart" id="loggedFullHeart" onClick={remLike} style={{display:"none"}}></i>
+                      <i className="far fa-heart" id="loggedOutEmptyHeart" style={{display:"none"}}></i>
+                      <i className="fas fa-heart" id="loggedOutFullHeart" style={{display:"none"}}></i>
+                      <span>{'\u00A0\u00A0'}{` ${allLikes?.length} ${allLikes?.length === 1 ? "like" : "likes"}`}</span>
+                    </tr>
                   </table>
                     {(currentUser?.id === currentUserId) &&
                       <NavLink to={`/stories/${storyId}/edit`}>
