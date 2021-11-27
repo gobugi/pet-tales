0d1ec6581c2ea0d95f8361ab764c03e4081d8693
johnny <johnpark1987@gmail.com>
2021-11-27 07:16:22 -0800

    still need to NavLink followers/following

--- frontend/src/components/ProfilePage/index.js
+++ frontend/src/components/ProfilePage/index.js
@@ -7,12 +7,14 @@ import './ProfilePage.css';
 import { getStories } from '../../store/stories';
 import { getUsers } from '../../store/users';
 import { restoreUser } from '../../store/session';
+import { csrfFetch } from "../../store/csrf";
 
 const ProfilePage = () => {
   const dispatch = useDispatch();
   const stories = useSelector((state) => state.stories);
   const storiesArr = Object.values(stories);
 
+  const sessionUser = useSelector(state => state.session.user);
 
   const users = useSelector((state) => state.users);
   const usersArr = Object.values(users);
@@ -28,13 +30,163 @@ const ProfilePage = () => {
 
   const myStories = storiesArr.filter((story) => story.authorId === +userId)
 
+  const [allFollows, setAllFollows] = useState([]);
+  const [currentFollow, setCurrentFollow] = useState([]);
+  const [followingUsernames, setFollowingUsernames] = useState([]);
+  const [followerUsernames, setFollowerUsernames] = useState([]);
+
+
+  useEffect(() => {
+
+    async function all_follows() {
+
+			const res = await fetch(`/api/users/`);
+			const resData = await res.json();
+
+      const followingUserIdsArr = [];
+      const followerUserIdsArr = [];
+
+			const response = await fetch(`/api/follows/`);
+			const responseData = await response.json();
+
+
+
+/////////////////////////////////////////////////////////
+      responseData?.forEach(follow => {
+        if (follow?.followerId === +userId) {
+          followingUserIdsArr?.push(follow?.userId)
+        }
+      })
+
+      const followingUsernamesArr = [];
+      resData?.forEach(user => {
+        if (followingUserIdsArr?.includes(user?.id)) {
+          followingUsernamesArr?.push(user?.username)
+        }
+      });
+
+      setFollowingUsernames(followingUsernamesArr?.sort(function(a, b) {
+        return a?.toLowerCase()?.localeCompare(b?.toLowerCase());
+      }))
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+      responseData?.forEach(follow => {
+        if (follow?.userId === +userId) {
+          followerUserIdsArr?.push(follow?.followerId)
+        }
+      })
+
+      const followerUsernamesArr = [];
+      resData?.forEach(user => {
+        if (followerUserIdsArr?.includes(user?.id)) {
+          followerUsernamesArr?.push(user?.username)
+        }
+      });
+
+      setFollowerUsernames(followerUsernamesArr?.sort(function(a, b) {
+        return a?.toLowerCase()?.localeCompare(b?.toLowerCase());
+      }))
+////////////////////////////////////////////////////
+
+
+
+      setAllFollows(responseData)
+
+      const currFollowArr = [];
+
+      responseData?.forEach(follow => {
+        (+userId === follow?.userId) && (sessionUser?.id === follow?.followerId) && currFollowArr?.push(follow);
+      })
+      setCurrentFollow(currFollowArr[0]);
+
+      if (!sessionUser) {
+        document.getElementById("followBtn")?.setAttribute("style","display:none;");
+        document.getElementById("unfollowBtn")?.setAttribute("style","display:none;");
+      }
+
+      if (sessionUser && currFollowArr?.length) {
+        document.getElementById("followBtn")?.setAttribute("style","display:none;");
+        document.getElementById("unfollowBtn")?.setAttribute("style","display:block;");
+      }
+
+      if (sessionUser && !currFollowArr?.length) {
+        document.getElementById("followBtn")?.setAttribute("style","display:block;");
+        document.getElementById("unfollowBtn")?.setAttribute("style","display:none;");
+      }
+
+		}
+
+    all_follows();
+	}, []);
+
+
+
+
+  //////////////// add follow ///////////////////
+  const addFollow = async (e) => {
+    e.preventDefault()
+
+    const newFollow = {
+      userId,
+      "followerId": currentUserId
+    }
+
+    const response = await csrfFetch(`/api/follows/`, {
+      method: 'POST',
+      body: JSON.stringify(newFollow),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+
+    if (response.ok) {
+
+      const data = await response.json();
+      setCurrentFollow(data)
+
+      const res = await fetch(`/api/follows/`);
+			const resData = await res.json();
+
+      setAllFollows(resData)
+
+      document.getElementById("followBtn")?.setAttribute("style","display:none;");
+      document.getElementById("unfollowBtn")?.setAttribute("style","display:block;");
+
+    }
+
+  }
+
+
+//////////////// remove follow ///////////////////
+const remFollow = async (e) => {
+  e.preventDefault();
+
+  const response = await csrfFetch(`/api/follows/${currentFollow?.id}`, {
+    method: 'DELETE'
+  });
+
+
+  if (response.ok) {
+    document.getElementById("followBtn")?.setAttribute("style","display:block;");
+    document.getElementById("unfollowBtn")?.setAttribute("style","display:none;");
+  }
+
+  setCurrentFollow([]);
+
+};
+
+
+
+
+
   useEffect(() => {
     dispatch(getStories());
     dispatch(getUsers());
     dispatch(restoreUser());
   }, [dispatch]);
 
-// console.log(storiesArr[0].id)
 
   return (
     <div className='profileDiv'>
@@ -48,8 +200,42 @@ const ProfilePage = () => {
             </div>
         </div>
       </div>
+
+      {currentUserId === +userId &&
+          <div id="followDivContainer">
+            <span id="followingDiv">
+              <strong>Following - </strong>
+              {followingUsernames?.length && followingUsernames?.map((username, i) => (i === followingUsernames?.length - 1 ? `${username}` : `${username}, `))}
+            </span>
+            <span id="followersDiv">
+              <strong>Followers - </strong>
+              {followerUsernames?.length && followerUsernames?.map((username, i) => (i === followerUsernames?.length - 1 ? `${username}` : `${username}, `))}
+            </span>
+          </div>
+      }
+
       <div className="profileStoriesContainer">
         <h2 className="profileStoriesTitle">~ {currentUserId === +userId ? 'My' : `${author}'s` } stories ~</h2>
+
+        {currentUserId !== +userId &&
+          <button id="followBtn" onClick={addFollow} style={{display:"none"}}>Follow</button>
+        }
+        {currentUserId !== +userId &&
+          <button id="unfollowBtn" onClick={remFollow} style={{display:"none"}}>Unfollow</button>
+        }
+        {currentUserId !== +userId &&
+          <br />
+        }
+        {currentUserId !== +userId &&
+          <hr />
+        }
+        {currentUserId !== +userId &&
+          <br />
+        }
+        {currentUserId !== +userId &&
+          <br />
+        }
+
         {(currentUserId === +userId) &&
         <NavLink id="newStoryNav" to='/stories/new'>
           <i className="fas fa-plus-circle fa-3x">
