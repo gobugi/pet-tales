557d610d3eb86716302385bd8b8f3507b08bf7ff
johnny <johnpark1987@gmail.com>
2021-11-27 02:52:06 -0800

    fixed cascade delete

--- backend/db/models/story.js
+++ backend/db/models/story.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
   Story.associate = function(models) {
     Story.belongsTo(models.User, { foreignKey: 'authorId' });
     Story.hasMany(models.Comment, { foreignKey: 'storyId', onDelete: 'CASCADE', hooks: true });
-    Story.hasMany(models.Like, { foreignKey: 'storyId' });
+    Story.hasMany(models.Like, { foreignKey: 'storyId', onDelete: 'CASCADE', hooks: true });
   };
   return Story;
 };
